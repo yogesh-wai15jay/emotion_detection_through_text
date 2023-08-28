fd2e238242a9af76a01273bc04093374b9389b34
yogesh-wai15jay <121447264+yogesh-wai15jay@users.noreply.github.com>
2023-08-28 13:52:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import base64
+
+import joblib
+
+pipe_lr = joblib.load(open("text_emotion.pkl", "rb"))
+
+emotions_emoji_dict = {"anger": "😠", "disgust": "🤮", "fear": "😨😱", "happy": "🤗", "joy": "😂", "neutral": "😐", "sad": "😔",
+                       "sadness": "😔", "shame": "😳", "surprise": "😮"}
+
+
+def predict_emotions(docx):
+    results = pipe_lr.predict([docx])
+    return results[0]
+
+
+def get_prediction_proba(docx):
+    results = pipe_lr.predict_proba([docx])
+    return results
+
+
+def main():
+    st.title("Text Emotion Detection")
+    st.subheader("Detect Emotions In Text")
+
+    with st.form(key='my_form'):
+        raw_text = st.text_area("Type Here")
+        submit_text = st.form_submit_button(label='Submit')
+
+    if submit_text:
+        col1, col2 = st.columns(2)
+
+        prediction = predict_emotions(raw_text)
+        probability = get_prediction_proba(raw_text)
+
+        with col1:
+            st.success("Original Text")
+            st.write(raw_text)
+
+            st.success("Prediction")
+            emoji_icon = emotions_emoji_dict[prediction]
+            st.write("{}:{}".format(prediction, emoji_icon))
+            st.write("Confidence:{}".format(np.max(probability)))
+
+        with col2:
+            st.success("Prediction Probability")
+            #st.write(probability)
+            proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
+            #st.write(proba_df.T)
+            proba_df_clean = proba_df.T.reset_index()
+            proba_df_clean.columns = ["emotions", "probability"]
+
+            fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions', y='probability', color='emotions')
+            st.altair_chart(fig, use_container_width=True)
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('original1.jpg')    
+
+
+
+if __name__ == '__main__':
+    main()
